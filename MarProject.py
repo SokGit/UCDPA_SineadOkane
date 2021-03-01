5fa47390d76b00428175c4344f4143e2d3e74c33
GitSok <soksinead.okane@outlook.com>
2021-03-01 19:18:13 +0000

    'Peeking At Data'

--- MarProject.py
+++ MarProject.py
@@ -2,3 +2,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+Sydney_HousePrices=pd.read_csv(r'C:\Users\soksi\OneDrive\Desktop\FinalProject\SydneyHousePrices.csv')
+#Reviwing Data with Head, tail and Describe.
+print(Sydney_HousePrices.head(5))
+print(Sydney_HousePrices.describe(include=['float','object']))
+
+
+
+
+
