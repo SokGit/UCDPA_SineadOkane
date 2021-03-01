eeecc0dac7db6a6551bba7bfd185a57f9281ac7f
GitSok <soksinead.okane@outlook.com>
2021-03-01 21:24:49 +0000

    'Drop Duplicates of Sell Prices'

--- MarProject.py
+++ MarProject.py
@@ -2,10 +2,33 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-Sydney_HousePrices=pd.read_csv(r'C:\Users\soksi\OneDrive\Desktop\FinalProject\SydneyHousePrices.csv')
-#Reviwing Data with Head, tail and Describe.
-print(Sydney_HousePrices.head(5))
-print(Sydney_HousePrices.describe(include=['float','object']))
+Sydney_HP=pd.read_csv(r'C:\Users\soksi\OneDrive\Desktop\FinalProject\SydneyHousePrices.csv')
+#Reviwing Data with Head,Describe and Info.
+print(Sydney_HP.head(5))
+print(Sydney_HP.describe(include=['float','object']))
+print(Sydney_HP.info())
+print(Sydney_HP.shape)
+print(Sydney_HP.values)
+print(Sydney_HP.columns)
+print(Sydney_HP.index)
+#Sorting in ascending order.
+print(Sydney_HP.sort_values('sellPrice'))
+#Sorting by multiple variables
+print(Sydney_HP.sort_values(['sellPrice','suburb']))
+print(Sydney_HP['sellPrice'].mean())
+print(Sydney_HP['sellPrice'].max())
+#Subsetting based on Higher End of Market 2 Billion
+HighMarket=Sydney_HP[Sydney_HP['sellPrice']>2000000000]
+print(HighMarket)
+#How Many Suburbs
+print(Sydney_HP['suburb'].value_counts())
+print(Sydney_HP.groupby('suburb')['sellPrice'].agg([max]))
+#Drop Duplicate Sell Prices&Call Sydney_Hp1
+Sydney_Hp1=Sydney_HP.drop_duplicates(subset='sellPrice')
+print(Sydney_Hp1)
+
+
+
 
 
 
